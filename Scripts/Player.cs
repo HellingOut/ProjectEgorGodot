70716b8229836bf0589b625a8c91054dbb8231e2
DheeD <zhulanoff.ilia@yandex.ru>
2025-03-11 14:46:30 +0400

    Completed basic movement

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -4,8 +4,8 @@ using Godot;
 
 public partial class Player : CharacterBody2D
 {
-	AnimatedSprite2D sprite = null;
-	CollisionShape2D collision = null;
+	AnimatedSprite2D _sprite = null;
+	CollisionShape2D _collision = null;
 	float _direction = 0;
 	[Export] public float TopSpeed = 300;
 	[Export] public float Acceleration = 0.1f;
@@ -15,51 +15,82 @@ public partial class Player : CharacterBody2D
 
 	public override void _Ready()
 	{
-		sprite = GetNode<AnimatedSprite2D>("Sprite");
-		sprite = GetNode<AnimatedSprite2D>("Sprite");
+		_sprite = GetNode<AnimatedSprite2D>("Sprite");
 	}
 
-	void Move(ref Vector2 localVelocity, double delta){
+	void YAnimationsPlay(Vector2 vel)
+	{
+		if (vel.Y < 0)
+		{
+			_sprite.Play("jump");
+		}
+		else
+		{
+			_sprite.Play("fall");
+		}
+	}
+
+	void GravityApply(ref Vector2 localVelocity, double delta)
+	{
+		localVelocity.Y += Gravity * (float)delta;
+	}
+
+	void Move(ref Vector2 localVelocity, double delta)
+	{
 		_direction = Input.GetAxis("walk_left", "walk_right");
 
-		if(_direction != 0){
+		if (_direction != 0)
+		{
 			localVelocity.X = Mathf.Lerp(localVelocity.X, TopSpeed * _direction, Acceleration * (float)delta);
-			sprite.Scale = new Vector2(_direction, 1);
-			sprite.Play("walk");
+			_sprite.Scale = new Vector2(_direction, 1);
+			_sprite.Play("walk");
 		}
-		else if(_direction == 0){
-			sprite.Play("idle");
+		else if (_direction == 0)
+		{
+			_sprite.Play("idle");
 			localVelocity.X = Mathf.Lerp(localVelocity.X, 0, Braking * (float)delta);
 		}
 	}
-	void Jump(ref Vector2 localVelocity){
-		if (IsOnFloor()){ 
-			if(Input.IsActionJustPressed("jump")){
-				localVelocity.Y = -JumpPower;
-			}
+	void Jump(ref Vector2 localVelocity)
+	{
+		if (Input.IsActionPressed("jump"))
+		{
+			localVelocity.Y = -JumpPower;
 		}
-		
 	}
+
+	bool FallThrough()
+	{
+		if (Input.IsActionPressed("fall_through"))
+		{
+			SetCollisionMaskValue(2, false);
+			return true;
+		}
+		return false;
+	}
+
+	void RecoverMask()
+	{
+		SetCollisionMaskValue(2, true);
+	}
+
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 localVelocity = Velocity;
-
 		Move(ref localVelocity, delta);
-		Jump(ref localVelocity);
-		
-		if (!IsOnFloor()){
-			localVelocity.Y += Gravity * (float)delta;
-			if(localVelocity.Y < 0)
-			{
-				sprite.Play("jump");
-			}
-			else{
-				sprite.Play("fall");
-			}
+
+		if (!IsOnFloor())
+		{
+			YAnimationsPlay(localVelocity);
+			GravityApply(ref localVelocity, delta);
 		}
-		
+		else
+		{
+			Jump(ref localVelocity);
+			if (!FallThrough())
+				RecoverMask();
+		}
 		Velocity = localVelocity;
 		MoveAndSlide();
-		
 	}
 }
